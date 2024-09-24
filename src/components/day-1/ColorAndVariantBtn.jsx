9f417c89139e9e40240dfffbe421c2cc0c4a0bca
Akash Harale <akash.v.harale@gmail.com>
2024-09-24 16:35:43 +0530

    day-1 Closes#1

--- src/components/day-1/ColorAndVariantBtn.jsx
+++ src/components/day-1/ColorAndVariantBtn.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ColorAndVariantBtn = () => {
   return (
       <div>
-          <Button color='default' variant='solid' size='large'>solid</Button>
+          <Button color='default' variant='solid' size='large'>solid</Button> <br />
     </div>
   )
 }
